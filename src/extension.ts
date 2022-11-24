c091eb238ad497e84e59aba4d3a4910234599293
neumaennl <neumaennl@gmx.net>
2022-11-24 11:25:44 +0100

    basic POC editor
    to display the content of the opened xsd file

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 import * as vscode from 'vscode';
 import { CatScratchEditorProvider } from './catScratchEditor';
 import { PawDrawEditorProvider } from './pawDrawEditor';
+import { VisualXmlSchemaEditorProvider } from './visualXmlSchemaEditor';
 
 export function activate(context: vscode.ExtensionContext) {
 	// Register our custom editor providers
 	context.subscriptions.push(CatScratchEditorProvider.register(context));
 	context.subscriptions.push(PawDrawEditorProvider.register(context));
+	context.subscriptions.push(VisualXmlSchemaEditorProvider.register(context));
 }
